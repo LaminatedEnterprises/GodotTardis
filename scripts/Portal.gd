0e4c2075a8b1759d9716cccf030b852f2929319a
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-02-25 22:32:28 +0000

    Portals work

--- scripts/Portal.gd
+++ scripts/Portal.gd
@@ -1,7 +1,10 @@
 extends MeshInstance3D
+class_name Portal3D
 
 @export
 var player: Player3D
+@export
+var other_portal: Portal3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +13,10 @@ func _ready() -> void:
 	if player.get_camera().environment != null:
 		$SubViewport/Camera3D.environment = player.get_camera().environment.duplicate()
 	elif get_tree().root.find_child("WorldEnvironment", true, false) != null:
-		$SubViewport/Camera3D.environment = get_tree().root.find_child("WorldEnvironment", true).environment.duplicate()
+		$SubViewport/Camera3D.environment = (get_tree().root.find_child("WorldEnvironment", true, false) as WorldEnvironment).environment.duplicate()
 	
 	$SubViewport/Camera3D.environment.tonemap_mode = Environment.TONE_MAPPER_LINEAR
+	$SubViewport.size = get_tree().root.get_viewport().size
+	
+func _process(delta: float) -> void:
+	$SubViewport/Camera3D.global_transform = other_portal.global_transform * (global_transform * Transform3D(Basis.from_euler(Vector3(0, PI, 0)), Vector3.ZERO)).affine_inverse() * player.get_camera().global_transform
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -2,9 +2,9 @@ extends CharacterBody3D
 class_name Player3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+@export var SPEED: float = 5.0
+@export var JUMP_VELOCITY: float = 4.5
+@export var yaw_speed: float = 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,12 +12,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -27,6 +27,15 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	
+	rotate_y(rel_motion.x * yaw_speed)
+	rel_motion = Vector2.ZERO
+	
+var rel_motion = Vector2.ZERO
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rel_motion = event.relative
 
 func get_camera() -> Camera3D:
 	return $Camera3D
